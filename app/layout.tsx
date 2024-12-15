9414d95d088028a039aea8036eeb39830260b262
root <root@VM-031.skj.nttdata-hokuriku.co.jp>
2024-12-15 13:06:48 +0900

    ログインとTODO画面を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
+import "@aws-amplify/ui-react/styles.css";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,14 +14,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <ConfigureAmplifyClientSide />
         {children}
       </body>
     </html>
