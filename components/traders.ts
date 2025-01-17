891c5e2430fc791ea316e2ad29773baca8abaf1d
root <root@VM-031.skj.nttdata-hokuriku.co.jp>
2025-01-17 17:08:17 +0900

    更新を追加

---
+++ components/traders.ts
@@ -0,0 +1,38 @@
+export const traders = [
+    {    
+        id: 'trader-1',
+        name: 'スノープラス金沢',
+    },
+    {    
+        id: 'trader-2',
+        name: 'ライトウェイ環境サービス',
+    },
+    {    
+        id: 'trader-3',
+        name: '株式会社ホワイトセーフティ',
+    },
+    {    
+        id: 'trader-4',
+        name: '株式会社ハイランド雪処理',
+    },
+    {    
+        id: 'trader-5',
+        name: 'ライトスノープロジェクト',
+    },
+    {    
+        id: 'trader-6',
+        name: '北陸スノーマスターズ',
+    },
+    {    
+        id: 'trader-7',
+        name: '片山メンテナンスサービス',
+    },
+    {    
+        id: 'trader-8',
+        name: '株式会社雪友プロジェクト',
+    },
+    {    
+        id: 'trader-9',
+        name: 'スノーバランス金沢',
+    },
+]
